3934d65f36e83e6d6d85f3cba62f28820336bf20
Mugil99 <mugilya99@gmail.com>
2023-06-15 21:28:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //generate states of India
+ 
+  const states=["AndhraPradesh", "ArunachalPradesh", "Assam", 'Bihar',
+   "Chhattisgarh", "Goa", "Gujarat", "Haryana", "HimachalPradesh", "Karnataka",
+    "Kerala", "MadhyaPradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram" ,
+    "Nagaland" ,"Odisha" ,"Punjab" ,"Rajasthan" ,"Sikkim" ,"TamilNadu" ,"Telangana"
+     ,"Tripura","UttarPradesh","Uttarakhand","WestBengal"]
+const[name,setName]=useState("");
+const[filteredState,setFilteredState]=useState([])    
+function showState(input){
+       setName(input)
+      const newArr=states.filter((state)=>state.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
+      console.log("newarr is ",newArr)
+      setFilteredState(newArr)
+}     
+
+function result(state){
+    setName(state);
+    setFilteredState([]);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+       <input value={name} onChange={(e)=>showState(e.target.value)} placeholder='Enter states of India'></input>
+       <div className={name?"box":""}>
+        {
+          filteredState.map((state,index)=>(
+            name&&(<div className='state' onClick={()=>result(state)}>
+              {state}
+            </div>)
+          ))
+        }
+       </div>
+   </div>
   );
 }
 
